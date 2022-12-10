897fde10fd84eb4d8c4c78e36c7a9360824ab6d0
frandiru <frandiazruiz@uma.es>
2022-12-10 18:11:16 +0100

    Se han añadido los ficheros con los templates html, así como el css para el diséño y las nuevas funcionalidades para el registro logeo y acceso anonimo

---
+++ app.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from markupsafe import escape
+from search4web import search4letters, log_request
+import sqlite3
+import databaseFunction
+
+app = Flask(__name__)
+
+@app.route("/") #Ruta de la que quiero que cuelgue esta función
+def hello_world() -> '302':  #Es un mensaje de redirección
+   return redirect('/login') #Inicia y termina un parrafo en HTML
+
+@app.route("/search", methods = ['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']  # Request es un diccionario y se accede a sus campos como ello
+    letters = request.form['letters']
+    user = request.form['user']
+    result = str(search4letters(phrase,letters))
+    #log_request(request,result) Para tener un archivo log, pero lo hemos cambiado por la base de datos
+    databaseFunction.log_request_db(request,result,user)
+    return render_template('results.html', the_title='Here are you results: ', the_phrase=phrase,
+                           the_letters=letters, the_results=result)
+
+@app.route("/entryAnonymous")
+def entry_page() -> 'html': #Se le ha dicho que devulve un html
+    return render_template('entry.html', the_title='Welcome to search for letter on web, your user is Anonymous', the_user = 'Anonymous')
+
+#Esta parte eliminarla no es normal que este en paginas web solo como depuración
+@app.route("/viewlog")
+def show_log() -> 'html':
+    #Si se guarda el log en un archivo en la base de datos
+    #with open('vsearch.log') as folder:
+    #    contents = folder.read()
+    connection = sqlite3.connect('BaseDataProyectos.sqlite')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT* FROM log""")
+    contents = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return escape(contents)
+
+
+@app.route("/login")
+def login() -> 'html':
+    return render_template('login.html', the_hour = actual())
+
+@app.route("/register")
+def register() -> 'html':
+    return render_template('register.html')
+
+@app.route("/registerComplete", methods = ['POST'])
+def registerComplete() -> 'html':
+    if databaseFunction.buscarUsuario(request.form['user']):
+        #El usuario existe
+        return render_template('registerComplete.html', result_register='USER ALREADY EXISTS. REGISTER IS NOT COMPLETE')
+    else:
+        #El usuario no existe
+        databaseFunction.insertarUsuario(request.form['user'],request.form['password'], request.form['email'])
+        return render_template('registerComplete.html', result_register = 'COMPLETE REGISTER')
+
+@app.route("/access", methods = ['POST'])
+def access() -> 'html':
+    if databaseFunction.validLogin(request.form['user'],request.form['password']):
+        return render_template('entry.html', the_title = 'Welcome ' + request.form['user'] + " to search for letter on web", the_user = request.form['user'])
+    else:
+        return render_template('error.html', the_title = "ERROR WITH THE NAME OF USER OR PASSWORD")
+
+def actual():
+    now = datetime.now() # Actualizar
+    hora = now.hour #hora actual
+    minuto = now.minute # minuto actual
+    segundo = now.second # segundo actual
+    if (hora < 10):  #dos cifras para la hora
+        hora="0"+hora
+    if (minuto < 10): #dos cifras para el minuto
+        minuto="0"+minuto
+    if (segundo < 10):  #dos cifras para el segundo
+        segundo="0"+segundo
+    mireloj = str(hora) + " : " + str(minuto) + " : " + str(segundo);
+    return mireloj
